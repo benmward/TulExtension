88dcc6c1530d40eec8f9da07eb4d0be40c83a310
grasbergerm <mgrasber@gmu.edu>
2014-04-05 19:09:36 -0400

    Took out extra spacing

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@ $(document).ready( function () {
 	$('#siteEntryForm').submit( function (e) {
 		e.preventDefault();
 		var url = $("input[name=siteUrl]").val();
-		$('.sitelist').append('<div class="site"><a href='+url+'>'+url+'</a><img src=' + chrome.extension.getURL('x.png') + '></div><br>');
+		$('.sitelist').append('<div class="site"><a href='+url+'>'+url+'</a><img src=' + chrome.extension.getURL('x.png') + '></div>');
 	//$('span').on('mouseenter', function () {
 	//	$('.site img').show();
 	//}).on('mouseleave', function () {
