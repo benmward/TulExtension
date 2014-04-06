448f6741792c58dd56abd86d6d547e77caaaa79d
Nathan LaPierre <nathanl2012@gmail.com>
2014-04-05 22:10:56 -0400

    html reformatting, "x" behavior 1.1

--- popup.js
+++ popup.js
@@ -2,7 +2,13 @@ $(document).ready( function () {
 	$('#siteEntryForm').submit( function (e) {
 		e.preventDefault();
 		var url = $("input[name=siteUrl]").val();
-		$('.sitelist').append('<div class="site"><a href='+url+'>'+url+'</a><img src=' + chrome.extension.getURL('x.png') + '></div>');
+		$('.sitelist').append('<div class="site"><span id="linkX"><a href='+url+'>'+url+'</a><img src=' + chrome.extension.getURL('x.png') + '></span></div>');
+		$('a').mouseenter(function() {
+			$('#linkX img').show();
+		})
+		$('a').mouseleave(function() {
+			$('#linkX img').hide();
+		})
 	//$('span').on('mouseenter', function () {
 	//	$('.site img').show();
 	//}).on('mouseleave', function () {
