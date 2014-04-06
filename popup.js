28547c9de150407a16a150efec955ea8791cd091
Nathan LaPierre <nathanl2012@gmail.com>
2014-04-06 01:44:48 -0400

    js fixes mistakes in url input

--- popup.js
+++ popup.js
@@ -2,19 +2,39 @@ $(document).ready( function () {
 	$('#siteEntryForm').submit( function (e) {
 		e.preventDefault();
 		var url = $("input[name=siteUrl]").val();
-		$('.sitelist').append('<div class="site"><span id="linkX"><a href='+url+'>   '+url+'   </a><img src=' + chrome.extension.getURL('x.png') + '></span></div>');
-		$('.site img').hide();
-		$('.site').mouseenter(function() {
-			$('#linkX img').show();
-		})
-		$('.site').mouseleave(function() {
-			$('#linkX img').hide();
-		})
+		if(urlFix(url))
+		{
+			url = urlFix(url);
+			$('.sitelist').append('<div class="site"><span id="linkX"><a href='+url+'>   '+url+'   </a><img src=' + chrome.extension.getURL('x.png') + '></span></div>');
+			$('.site img').hide();
+			$('.site').mouseenter(function() {
+				$('#linkX img').show();
+			})
+			$('.site').mouseleave(function() {
+				$('#linkX img').hide();
+			})
+		}
 	//$('span').on('mouseenter', function () {
 	//	$('.site img').show();
 	//}).on('mouseleave', function () {
 	//	$('.site img').hide();
 	});
+	
+	var urlFix = function(url) {
+		var http = new RegExp("^https?:\\/\\/www\\.[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)$","g");
+		var www = new RegExp("^(https?:\\/\\/)?www\\.[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)$","g");
+		var valid = new RegExp(".+\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)$","g");
+		if(!valid.test(url))
+		{
+			alert("Invalid URL!");
+			return "";
+		}
+		else if (!www.test(url))
+			return "http://www."+url;
+		else if (!http.test(url))
+			return "http://"+url;
+		else return url;
+	};
 });
 
 $('body').on('click', 'a', function(){
